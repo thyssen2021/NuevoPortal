60a77cfc834031dd4018e5cf372a88a80b4092e2
thyssen2021 <89875155+thyssen2021@users.noreply.github.com>
2022-12-14 13:32:21 -0600

    2022-12-14 Se agregan clases para lectura de cfdi 3.3 y 4.0

---
+++ CódigoFuente/Bitacoras/Util/XmlUtil.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Bitacoras.Util
+{
+    static public class XmlUtil
+    {
+
+        static public Comprobante LeeXML_3_3(Stream stream)
+        {
+
+            //crear un objeto el cual tendrá el resultado final, este objeto es el principal
+            Comprobante oComprobante;
+            //pon la ruta donde tienes tu archivo XML Timbrado
+            //string path = @"C:\miXML.xml";
+
+            //creamos un objeto XMLSerializer para deserializar
+            XmlSerializer oSerializer = new XmlSerializer(typeof(Comprobante));
+
+            //creamos un flujo el cual recibe nuestro xml
+            using (StreamReader reader = new StreamReader(stream))
+            {      
+                //aqui deserializamos
+                oComprobante = (Comprobante)oSerializer.Deserialize(reader);              
+
+                //Deserializamos el complemento timbre fiscal
+                foreach (var oComplemento in oComprobante.Complemento)
+                {
+                    foreach (var oComplementoInterior in oComplemento.Any)
+                    {
+                        //si el complemento es TimbreFiscalDigital lo deserializamos
+                        if (oComplementoInterior.Name.Contains("TimbreFiscalDigital"))
+                        {
+
+                            //Objeto para aplicar ahora la deserialización del complemento timbre
+                            XmlSerializer oSerializerComplemento = new XmlSerializer(typeof(TimbreFiscalDigital));
+                            //creamos otro flujo para el complemento
+                            using (var readerComplemento = new StringReader(oComplementoInterior.OuterXml))
+                            {
+                                //y por ultimo deserializamos el complemento
+                                oComprobante.TimbreFiscalDigital =
+                                    (TimbreFiscalDigital)oSerializerComplemento.Deserialize(readerComplemento);
+                            }
+
+                        }
+                    }
+                }
+            }
+
+            return oComprobante;
+        }
+        static public  Bitacoras.CFDI_4_0.Comprobante LeeXML_4_0(Stream stream)
+        {
+
+            //crear un objeto el cual tendrá el resultado final, este objeto es el principal
+            Bitacoras.CFDI_4_0.Comprobante oComprobante;
+            //pon la ruta donde tienes tu archivo XML Timbrado
+            //string path = @"C:\miXML.xml";
+
+            //creamos un objeto XMLSerializer para deserializar
+            XmlSerializer oSerializer = new XmlSerializer(typeof(Bitacoras.CFDI_4_0.Comprobante));
+
+            //creamos un flujo el cual recibe nuestro xml
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                //aqui deserializamos
+                oComprobante = (Bitacoras.CFDI_4_0.Comprobante)oSerializer.Deserialize(reader);
+
+                //Deserializamos el complemento timbre fiscal
+                foreach (var oComplemento in oComprobante.Complemento)
+                {
+                    foreach (var oComplementoInterior in oComplemento.Any)
+                    {
+                        //si el complemento es TimbreFiscalDigital lo deserializamos
+                        if (oComplementoInterior.Name.Contains("TimbreFiscalDigital"))
+                        {
+
+                            //Objeto para aplicar ahora la deserialización del complemento timbre
+                            XmlSerializer oSerializerComplemento = new XmlSerializer(typeof(TimbreFiscalDigital));
+                            //creamos otro flujo para el complemento
+                            using (var readerComplemento = new StringReader(oComplementoInterior.OuterXml))
+                            {
+                                //y por ultimo deserializamos el complemento
+                                oComprobante.TimbreFiscalDigital =
+                                    (TimbreFiscalDigital)oSerializerComplemento.Deserialize(readerComplemento);
+                            }
+
+                        }
+                    }
+                }
+            }
+
+            return oComprobante;
+        }
+    }
+}
--- CódigoFuente/BitacorasProduccion/Controllers/ComprobacionGVController.cs
+++ CódigoFuente/BitacorasProduccion/Controllers/ComprobacionGVController.cs
@@ -3,10 +3,12 @@ using Clases.Util;
 using Portal_2_0.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml.Serialization;
 
 namespace Portal_2_0.Controllers
 {
@@ -197,7 +199,7 @@ namespace Portal_2_0.Controllers
             //determina si aplica otro o no
             gV_solicitud.medio_transporte_aplica_otro = !gV_solicitud.id_medio_transporte.HasValue;
             ViewBag.plantaClave = AddFirstItem(new SelectList(db.plantas.Where(p => p.activo == true), "clave", "descripcion"), textoPorDefecto: "-- Seleccione un valor --");
-            ViewBag.id_centro_costo =  AddFirstItem(new SelectList(db.GV_centros_costo.Where(p => p.activo == true), nameof(GV_centros_costo.id), nameof(GV_centros_costo.ConcatCentroDepto)), textoPorDefecto: "-- Seleccione un valor --");
+            ViewBag.id_centro_costo = AddFirstItem(new SelectList(db.GV_centros_costo.Where(p => p.activo == true), nameof(GV_centros_costo.id), nameof(GV_centros_costo.ConcatCentroDepto)), textoPorDefecto: "-- Seleccione un valor --");
 
             return View(gV_solicitud); ;
         }
@@ -210,11 +212,11 @@ namespace Portal_2_0.Controllers
         public ActionResult ComprobacionGastos(GV_solicitud solicitud)
         {
 
-           // ModelState.AddModelError("", "Error prueba.");
+            // ModelState.AddModelError("", "Error prueba.");
             if (ModelState.IsValid)
             {
                 TempData["Mensaje"] = new MensajesSweetAlert("Se ha creado el registro correctamente.", TipoMensajesSweetAlerts.SUCCESS);
-                return RedirectToAction("Solicitudes", new { estatus="EN_PROCESO"});
+                return RedirectToAction("Solicitudes", new { estatus = "EN_PROCESO" });
             }
 
             //En caso de que el modelo no sea válido
@@ -225,7 +227,7 @@ namespace Portal_2_0.Controllers
 
             //determina si aplica otro o no
             solicitud.medio_transporte_aplica_otro = !solicitud.id_medio_transporte.HasValue;
-     
+
             ViewBag.plantaClave = AddFirstItem(new SelectList(db.plantas.Where(p => p.activo == true), "clave", "descripcion"), textoPorDefecto: "-- Seleccione un valor --", selected: gV_ComprobacionRecibida.clave_planta.ToString());
             ViewBag.id_centro_costo = AddFirstItem(new SelectList(db.GV_centros_costo.Where(p => p.activo == true && p.clave_planta == solicitud.GV_comprobacion.clave_planta), nameof(GV_centros_costo.id), nameof(GV_centros_costo.ConcatCentroDepto)), textoPorDefecto: "-- Seleccione un valor --", selected: gV_ComprobacionRecibida.id_centro_costo.ToString());
 
@@ -297,6 +299,104 @@ namespace Portal_2_0.Controllers
             {
                 return View();
             }
+        }
+
+        /// <summary>
+        /// Método que lee el contenido de una factura xml
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public JsonResult ObtieneConceptos()
+        {
+            var result = new object[1];
+
+            HttpPostedFileBase file = Request.Files[0];
+
+            MemoryStream newStream = new MemoryStream();
+            file.InputStream.CopyTo(newStream);
+
+            //Valida que se haya enviado un archivo
+            if (file.FileName == "")
+            {
+                result[0] = new { status = "ERROR", value = "Seleccione un archivo." };
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+
+            //valida que sea un archivo en excel
+            string extension = Path.GetExtension(file.FileName);
+            if (extension.ToUpper() != ".XML")
+            {
+                result[0] = new { status = "ERROR", value = "Sólo se permiten archivos con extensión .xml." };
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+
+
+            #region LeeXML
+
+            string estatus = string.Empty;
+            string msj = string.Empty;
+
+            Comprobante factura_3_3 = null;
+            Bitacoras.CFDI_4_0.Comprobante factura_4_0 = null;
+            //primero trata de leer la factura 3.3
+            try
+            {
+                file.InputStream.Position = 0;
+                factura_3_3 = XmlUtil.LeeXML_3_3(file.InputStream);
+            }
+            catch (Exception e)
+            {
+                //si falla al leer la 3.3, trata de leer la 4.0
+                try
+                {
+                    newStream.Position = 0;
+                    factura_4_0 = XmlUtil.LeeXML_4_0(newStream);
+                }
+                catch (Exception ex)
+                {
+                    estatus = "ERROR";
+                    msj = "Ocurrió un error al leer la estructura del XML: " + ex.Message;
+                }
+
+            }
+
+            #endregion            
+
+
+
+            //si hubo algún error al leer el archivo
+            if (estatus.ToUpper() == "ERROR")
+            {
+                result[0] = new { status = estatus, value = msj };
+                return Json(result, JsonRequestBehavior.AllowGet);
+            }
+            else
+            { //si el archivo se lee correctamente
+
+                string body = string.Empty;
+                string div_ocultos = string.Empty;
+
+                if (factura_3_3 != null)
+                {
+                    estatus = "OK";
+                    body = "Factura Versión: " + factura_3_3.Version;
+                }
+                else if (factura_4_0 != null) {
+                    estatus = "OK";
+                    body = "Factura Versión: " + factura_4_0.Version;
+                }
+
+                /*
+                 * enviar el contenido del body de la tabla
+                 * Aumenta la longitud máxima por defecto del serializador
+                 */
+
+                result[0] = new { status = estatus, value = body, div_ocultos = div_ocultos };
+                var json = Json(result, JsonRequestBehavior.AllowGet);
+                return json;
+            }
+
+
         }
     }
 }
