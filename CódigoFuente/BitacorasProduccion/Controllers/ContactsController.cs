1d678a3613c91681d63112d3dbf58f1ea2a8ead1
thyssen2021 <89875155+thyssen2021@users.noreply.github.com>
2023-02-22 11:28:26 -0600

    2023-02-22 Se agrega filtro de teléfono a directorio.

--- CódigoFuente/BitacorasProduccion/Controllers/ContactsController.cs
+++ CódigoFuente/BitacorasProduccion/Controllers/ContactsController.cs
@@ -19,7 +19,7 @@ namespace Portal_2_0.Controllers
 
         // GET: Contacts
         [AllowAnonymous]
-        public ActionResult Index(string nombre, string num_empleado, int? id_jefe_directo, int? id_area, int planta_clave = 0, int pagina = 1)
+        public ActionResult Index(string nombre, int? id_area, bool? incluye_telefono, bool? busqueda, int planta_clave = 0, int pagina = 1)
         {
 
             //mensaje en caso de crear, editar, etc
@@ -28,41 +28,53 @@ namespace Portal_2_0.Controllers
                 ViewBag.MensajeAlert = TempData["Mensaje"];
             }
 
+            //verifica si debe establecer como null el area
             if (!db.Area.Any(x => x.clave == id_area && x.plantaClave == planta_clave))
                 id_area = null;
 
+            //establece el valor por defecto para incluye_telefono
+            if (!busqueda.HasValue)
+                incluye_telefono = true;
+
             var cantidadRegistrosPorPagina = 12; // parámetro
 
-            var listado = db.empleados
-                   .Where(x =>
-                       ((x.nombre + " " + x.apellido1 + " " + x.apellido2).Contains(nombre) || String.IsNullOrEmpty(nombre))
-                       && (x.numeroEmpleado.Contains(num_empleado) || String.IsNullOrEmpty(num_empleado))
-                       && (x.planta_clave == planta_clave || planta_clave == 0)
-                       && (id_area==null || x.id_area == id_area)
-                       && (x.activo.HasValue && x.activo.Value) //si está activo
-                       && (id_jefe_directo == null || x.id_jefe_directo == id_jefe_directo)
-                       )
-                   .OrderBy(x => x.id)
-                   .Skip((pagina - 1) * cantidadRegistrosPorPagina)
-                  .Take(cantidadRegistrosPorPagina).ToList();
+            //primero obtiene el listado de empleado sin el filtro de líneas
 
-            var totalDeRegistros = db.empleados
+            List<empleados> listado = new List<empleados>();
+
+            var listadoTemporal = db.empleados
                    .Where(x =>
                        ((x.nombre + " " + x.apellido1 + " " + x.apellido2).Contains(nombre) || String.IsNullOrEmpty(nombre))
-                       && (x.numeroEmpleado.Contains(num_empleado) || String.IsNullOrEmpty(num_empleado))
                        && (x.planta_clave == planta_clave || planta_clave == 0)
                        && (id_area == null || x.id_area == id_area)
                        && (x.activo.HasValue && x.activo.Value) //si está activo
-                       && (id_jefe_directo == null || x.id_jefe_directo == id_jefe_directo)
-                       )
-                .Count();
+                       ).ToList();
+
+            //recorre todos los empleados y determina quién si tiene lineas o no        
+            foreach (var e in listadoTemporal)
+            {
+                if (incluye_telefono == null || (!String.IsNullOrEmpty(e.extension) || e.GetIT_Inventory_Cellular_LinesActivas().Count > 0))
+                    listado.Add(e);
+            }
+
+            //debe ir antes de obtener unicamente tomar los registros de las páginas
+            var totalDeRegistros = listado
+                 .Count();
+
+            listado = listado
+            .OrderBy(x => x.id)
+            .Skip((pagina - 1) * cantidadRegistrosPorPagina)
+           .Take(cantidadRegistrosPorPagina).ToList();
+
+           
 
             //para paginación
 
             System.Web.Routing.RouteValueDictionary routeValues = new System.Web.Routing.RouteValueDictionary();
             routeValues["nombre"] = nombre;
-            routeValues["planta_clave"] = planta_clave;            
-            routeValues["id_area"] = id_area;            
+            routeValues["planta_clave"] = planta_clave;
+            routeValues["id_area"] = id_area;
+            routeValues["incluye_telefono"] = incluye_telefono;
 
             Paginacion paginacion = new Paginacion
             {
@@ -73,11 +85,8 @@ namespace Portal_2_0.Controllers
             };
 
             ViewBag.planta_clave = AddFirstItem(new SelectList(db.plantas.Where(p => p.activo == true), "clave", "descripcion", planta_clave.ToString()), textoPorDefecto: "-- Todas --");
-            ViewBag.id_area = AddFirstItem(new SelectList(db.Area.Where(p => p.activo == true && ( p.plantaClave == planta_clave)), nameof(Area.clave), nameof(Area.descripcion), planta_clave.ToString()), textoPorDefecto: "-- Todas --");
+            ViewBag.id_area = AddFirstItem(new SelectList(db.Area.Where(p => p.activo == true && (p.plantaClave == planta_clave)), nameof(Area.clave), nameof(Area.descripcion), planta_clave.ToString()), textoPorDefecto: "-- Todas --");
             ViewBag.Paginacion = paginacion;
-
-            ViewBag.id_jefe_directo = AddFirstItem(new SelectList(db.empleados.Where(x => x.activo == true), nameof(empleados.id), nameof(empleados.ConcatNumEmpleadoNombre)),
-           textoPorDefecto: "-- Seleccione un valor --", selected: id_jefe_directo.ToString());
 
             return View(listado);
 
