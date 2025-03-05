3fdf41e78ca80bf082667419d50334c2b4fc8aaf
thyssen2021 <89875155+thyssen2021@users.noreply.github.com>
2025-03-05 09:38:05 -0600

    2024-03-05 Se agrega video de scrap.

---
+++ CódigoFuente/BitacorasProduccion/Controllers/VideosController.cs
@@ -0,0 +1,23 @@
+﻿using Portal_2_0.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portal_2_0.Controllers
+{
+    public class VideosController : BaseController
+    {
+        // GET: Videos
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GeneracionScrap()
+        {
+            return View();
+        }
+
+    }
+}
