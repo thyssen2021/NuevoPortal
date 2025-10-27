e25d55f4565b3e30121f53f7685f92c30140e0f9
PANI AGUILAR <10740494@thyssenkrupp-materials.com>
2025-10-27 12:45:42 -0600

    2025/10/27: Ejecución Inicial

---
+++ CódigoFuente/BitacorasProduccion/Models/MetadataModels/budget_rel_conceptos_cantidades_forecastMetadata.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Portal_2_0.Models
+{
+    public class budget_rel_conceptos_cantidades_forecastMetadata
+    {
+
+    }
+
+    [MetadataType(typeof(budget_rel_conceptos_cantidades_forecastMetadata))]
+    public partial class budget_rel_conceptos_cantidades_forecast
+    {
+        //para binding de ajax
+        public string clave { get; set; }
+        public string cuenta_sap { get; set; }
+        public int id_rel_fy_centro { get; set; }
+        public int mes { get; set; }
+        public string currency { get; set; }
+    }
+}
