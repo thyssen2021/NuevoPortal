4147caac8c34a385f2c3ce8b9ad949b8230fa04d
AlfredoX <alfredo.xochitemol@thyssenkrupp-materials.com>
2025-10-31 13:11:22 -0600

    2025-10-31 Se enlazan nuevas tablas de materiales en SCDM.

---
+++ CódigoFuente/BitacorasProduccion/Models/Auxiliares/SAPCharacteristics.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portal_2_0.Models.Auxiliares
+{
+    public enum SAPCharacteristics
+    {
+        COMMODITY,
+        GRADE,
+        SHAPE,
+        SURFACE,
+        COATING_WEIGHT,
+        WIDTH_M,
+        GAUGE_M,
+        FLATNESS_M,
+        SURFACE_TREATMENT,
+        MILL,
+        CUSTOMER_NUMBER,
+        CUSTOMER_PART_NUMBER,
+        CUSTOMER_PART2,
+        LENGTH_M,
+        ID_COIL_M,
+        OD_COIL_M
+    }
+
+    public static class EnumExtensions
+    {
+        public static string GetCharName(this Enum value)
+        {
+            // Esta función obtiene el nombre del miembro del enum.
+            return value.ToString();
+        }
+    }
+}
